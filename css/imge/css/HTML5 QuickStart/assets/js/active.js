36939f49210017cb8bd00be4d536616a233f5884
ashikulmoula <30575574+ashikulmoula@users.noreply.github.com>
2017-07-31 01:05:35 +0600

    Add files via upload

---
+++ css/imge/css/HTML5 QuickStart/assets/js/active.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+ var s = skrollr.init({
+     render: function(data){
+     //Debugging - log the current scroll position.
+     //Consol.log(data.curTop);
+        }
+ })
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
